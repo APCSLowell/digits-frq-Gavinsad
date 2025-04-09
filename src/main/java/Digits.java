460ddc7803ddafa55924be9de11907aceb2b1a8a
Gavinsad <gali6@s.sfusd.edu>
2025-04-09 11:44:52 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,25 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	         digitList = new ArrayList<Integer>;
+		if(num==0){digitList.add(0);}
+			   while(num>0){
+				digitList.add(0,n%10);
+				   num = num/10;
+
+			   }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+	if(digitList.size<=1){return true;}
+		int x = digitList.get(0);
+		for(int i=1;i<digitList.size();i++){
+if(digitList.get(i)<=x){return false;}
+			x=digitList.get(i);
+
+		}
+		return true;
 
 	}
 	
